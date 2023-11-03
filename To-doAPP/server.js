d469d901a193b870094914aaf27218675f513fee
gacenti <wjsrktks@gmail.com>
2023-11-03 16:55:02 +0900

    Added template, Changed html to ejs system, (gs)

--- To-doAPP/server.js
+++ To-doAPP/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
 const port = 8080;
+
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+
 app.listen(port, function(){
     console.log(`서버가 실행되고 있습니다. \n 접속주소 : http://localhost:${port}`)
 });
 
 app.get('/', function(req,res){
-    res.sendFile(__dirname + '/index.html');
+    res.render('home');
 });
 
 // 누군가가 /hello 로 방문을 하면..
@@ -19,4 +25,8 @@ app.get('/hello', function(req, res){
 
 app.get('/write', function(req,res){
     res.sendFile(__dirname+'/write.html');
+});
+
+app.get('/temp', function(req,res){
+    res.sendFile(__dirname+'/temp.html');
 });
